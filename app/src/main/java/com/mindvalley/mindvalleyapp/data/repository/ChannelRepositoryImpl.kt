62f0305f88f2e99e55ab1fa77a932d55b3bb7298
Ishtian Revee <ishtian11@gmail.com>
2024-07-16 01:27:55 +0600

    - channel repository implementation class added

---
+++ app/src/main/java/com/mindvalley/mindvalleyapp/data/repository/ChannelRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.mindvalley.mindvalleyapp.data.repository
+
+import com.mindvalley.mindvalleyapp.common.Resource
+import com.mindvalley.mindvalleyapp.data.local.AppDatabase
+import com.mindvalley.mindvalleyapp.data.remote.MindvalleyApi
+import com.mindvalley.mindvalleyapp.domain.model.Category
+import com.mindvalley.mindvalleyapp.domain.model.Channel
+import com.mindvalley.mindvalleyapp.domain.model.Media
+import com.mindvalley.mindvalleyapp.domain.repository.ChannelRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ChannelRepositoryImpl @Inject constructor(
+    val api: MindvalleyApi,
+    val database: AppDatabase
+): ChannelRepository {
+    override suspend fun getNewEpisodes(): Flow<Resource<List<Media>>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getChannels(): Flow<Resource<List<Channel>>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getCategories(): Flow<Resource<List<Category>>> {
+        TODO("Not yet implemented")
+    }
+}
