5d22e035feb6c8212cf45af649af0ba2f5e3cf49
Ishtian Revee <ishtian11@gmail.com>
2024-07-16 01:32:44 +0600

    - local data source class added

---
+++ app/src/main/java/com/mindvalley/mindvalleyapp/data/local/LocalDataSource.kt
@@ -0,0 +1,25 @@
+package com.mindvalley.mindvalleyapp.data.local
+
+import com.mindvalley.mindvalleyapp.data.model.CategoryEntity
+import com.mindvalley.mindvalleyapp.data.model.ChannelEntity
+import com.mindvalley.mindvalleyapp.data.model.MediaEntity
+import kotlinx.coroutines.flow.Flow
+
+class LocalDataSource(
+    private val channelDao: ChannelDao
+) {
+    fun getNewEpisodes(): Flow<List<MediaEntity>> = channelDao.getNewEpisodes()
+    suspend fun insertNewEpisodes(episodes: List<MediaEntity>): Array<Long> {
+        return channelDao.insertNewEpisodes(episodes)
+    }
+
+    fun getChannels(): Flow<List<ChannelEntity>> = channelDao.getChannels()
+    suspend fun insertChannels(channels: List<ChannelEntity>): Array<Long> {
+        return channelDao.insertChannels(channels)
+    }
+
+    fun getCategories(): Flow<List<CategoryEntity>> = channelDao.getCategories()
+    suspend fun insertCategories(categories: List<CategoryEntity>): Array<Long> {
+        return channelDao.insertCategories(categories)
+    }
+}
