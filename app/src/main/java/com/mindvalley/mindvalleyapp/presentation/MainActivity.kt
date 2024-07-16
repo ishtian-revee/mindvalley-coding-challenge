9eee423e51db2b64d6ccfd6f3b3dcc0458057a10
Ishtian Revee <ishtian11@gmail.com>
2024-07-16 21:28:06 +0600

    - surface added in the main app theme

--- app/src/main/java/com/mindvalley/mindvalleyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/mindvalley/mindvalleyapp/presentation/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.mindvalley.mindvalleyapp.presentation.theme.Dark
 import com.mindvalley.mindvalleyapp.presentation.theme.MindvalleyAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +16,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MindvalleyAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Dark
+                ) {
 
+                }
             }
         }
     }
