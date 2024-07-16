10942c030230b5370230a7a5b946db4a833f7f5d
Ishtian Revee <ishtian11@gmail.com>
2024-07-16 21:20:48 +0600

    - typography added

--- app/src/main/java/com/mindvalley/mindvalleyapp/presentation/theme/Type.kt
+++ app/src/main/java/com/mindvalley/mindvalleyapp/presentation/theme/Type.kt
@@ -2,33 +2,54 @@ package com.mindvalley.mindvalleyapp.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mindvalley.mindvalleyapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_bold)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+        lineHeight = 34.5.sp,
+        letterSpacing = 0.4.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontSize = 20.sp,
+        lineHeight = 24.76.sp,
+        letterSpacing = 0.14.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.13.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 17.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.4.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 19.6.sp,
+        letterSpacing = 0.11.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 13.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 1.sp
     )
-    */
 )
