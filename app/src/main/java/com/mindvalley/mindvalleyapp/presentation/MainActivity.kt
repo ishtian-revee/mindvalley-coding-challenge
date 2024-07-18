5451f41bc42330189ee9cbba10a81ab3d0fcbf1f
Ishtian Revee <ishtian11@gmail.com>
2024-07-18 10:56:41 +0600

    - edge to edge window disabled

--- app/src/main/java/com/mindvalley/mindvalleyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/mindvalley/mindvalleyapp/presentation/MainActivity.kt
@@ -16,7 +16,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MindvalleyAppTheme {
                 val viewModel = viewModel<ChannelViewModel>()
