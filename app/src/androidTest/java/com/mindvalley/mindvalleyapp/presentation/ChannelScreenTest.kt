2060ac266b82a4dcfb3a0d45e32568a84b7cabff
Ishtian Revee <ishtian11@gmail.com>
2024-07-24 20:25:29 +0600

    - simple screen text tests added

--- app/src/androidTest/java/com/mindvalley/mindvalleyapp/presentation/ChannelScreenTest.kt
+++ app/src/androidTest/java/com/mindvalley/mindvalleyapp/presentation/ChannelScreenTest.kt
@@ -2,12 +2,13 @@ package com.mindvalley.mindvalleyapp.presentation
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onAllNodesWithText
 import com.mindvalley.mindvalleyapp.R
 import com.mindvalley.mindvalleyapp.di.AppModule
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -15,17 +16,21 @@ import org.junit.Test
 @UninstallModules(AppModule::class)
 class ChannelScreenTest {
 
-//    @get:Rule
-//    val composeTestRule = createAndroidComposeRule<MainActivity>()
-//
-//    @Test
-//    fun channelContentTest() {
-//        composeTestRule.onAllNodesWithText(composeTestRule.activity.getString(R.string.channels))[0].assertIsDisplayed()
-//        composeTestRule.onAllNodesWithText(composeTestRule.activity.getString(R.string.new_episodes))[0].assertIsDisplayed()
-//        composeTestRule.onAllNodesWithText(composeTestRule.activity.getString(R.string.browse_by_categories))[0].assertIsDisplayed()
-//        composeTestRule.onAllNodesWithTag("episode_list")[0].assertIsDisplayed()
-//        composeTestRule.onAllNodesWithTag("channel_list")[0].assertIsDisplayed()
-//        composeTestRule.onAllNodesWithTag("category_list")[0].assertIsDisplayed()
-//    }
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
 
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun screenTextTest() {
+        composeRule.onAllNodesWithText(composeRule.activity.getString(R.string.channels))[0].assertIsDisplayed()
+        composeRule.onAllNodesWithText(composeRule.activity.getString(R.string.new_episodes))[0].assertIsDisplayed()
+        composeRule.onAllNodesWithText(composeRule.activity.getString(R.string.browse_by_categories))[0].assertIsDisplayed()
+    }
 }
