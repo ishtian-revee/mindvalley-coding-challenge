a2062730aa19e5afb08700b8092a069ba1cf46b2
Ishtian Revee <ishtian11@gmail.com>
2024-07-24 20:30:33 +0600

    - missing repository injection for use case classes added

--- app/src/main/java/com/mindvalley/mindvalleyapp/domain/use_case/GetCategoryUseCase.kt
+++ app/src/main/java/com/mindvalley/mindvalleyapp/domain/use_case/GetCategoryUseCase.kt
@@ -4,8 +4,9 @@ import com.mindvalley.mindvalleyapp.common.Resource
 import com.mindvalley.mindvalleyapp.domain.model.Category
 import com.mindvalley.mindvalleyapp.domain.repository.ChannelRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class GetCategoryUseCase(
+class GetCategoryUseCase @Inject constructor(
     private val channelRepository: ChannelRepository
 ) : CategoryUseCase {
     override suspend fun getCategories(): Flow<Resource<List<Category>>> =
--- app/src/main/java/com/mindvalley/mindvalleyapp/domain/use_case/GetChannelsUseCase.kt
+++ app/src/main/java/com/mindvalley/mindvalleyapp/domain/use_case/GetChannelsUseCase.kt
@@ -4,8 +4,9 @@ import com.mindvalley.mindvalleyapp.common.Resource
 import com.mindvalley.mindvalleyapp.domain.model.Channel
 import com.mindvalley.mindvalleyapp.domain.repository.ChannelRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class GetChannelsUseCase(
+class GetChannelsUseCase @Inject constructor(
     private val channelRepository: ChannelRepository
 ) : ChannelUseCase {
     override suspend fun getChannels(): Flow<Resource<List<Channel>>> =
